be7ebeb02dac1acee3b0a2a571a6938e7235206a
Jacob Millward <jacob@seatfrog.com>
2023-08-02 10:48:23 +0100

    Make `install_fish.sh` idempotent

--- macOS/install_fish.sh
+++ macOS/install_fish.sh
@@ -5,18 +5,35 @@
 # Description: Installs Fish shell with Homebrew and sets it as the default shell
 ################################################################################
 
-# Install Fish shell using Homebrew
-echo "Installing Fish shell using Homebrew..."
-brew install fish
-
-# Add Fish shell to the list of allowed shells
-echo "Adding Fish shell to the list of allowed shells..."
-if ! grep -q "/usr/local/bin/fish" /etc/shells; then
-    echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+# Check if Fish shell is already installed
+if command -v fish >/dev/null 2>&1; then
+    echo "Fish shell already installed!"
+else
+    # Install Fish shell using Homebrew
+    echo "Installing Fish shell using Homebrew..."
+    brew install fish
+    echo -n "✔"
 fi
-echo -n "✔"
 
-# Set Fish shell as the default shell for the current user
-echo "Setting Fish shell as the default shell for the current user..."
-chsh -s /usr/local/bin/fish
-echo -n "✔"
+BREW_PREFIX=$(brew --prefix)
+FISH_PATH="$BREW_PREFIX/bin/fish"
+
+# Check if Fish shell is already in the list of allowed shells
+if grep -q "$FISH_PATH" /etc/shells; then
+    echo "Fish shell already in the list of allowed shells!"
+else
+    # Add Fish shell to the list of allowed shells
+    echo "Adding Fish shell to the list of allowed shells..."
+    echo "$FISH_PATH" | sudo tee -a /etc/shells
+    echo -n "✔"
+fi
+
+# Check if Fish shell is already the default shell for the current user
+if [[ "$(dscl . -read /Users/$(whoami) UserShell | awk '{print $2}')" == "$FISH_PATH" ]]; then
+    echo "Fish shell already the default shell for the current user!"
+else
+    # Set Fish shell as the default shell for the current user
+    echo "Setting Fish shell as the default shell for the current user..."
+    chsh -s "$FISH_PATH"
+fi
+    echo -n "✔"
